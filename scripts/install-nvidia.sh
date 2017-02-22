380938fea0c853330972a7c6070be0de6fff983b
James Croft <james.r.croft@googlemail.com>
2017-02-22 08:45:01 +0000

    Update nvidia-docker install

--- scripts/install-nvidia.sh
+++ scripts/install-nvidia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Based on Jonathan Calmels approachhttps://github.com/NVIDIA/nvidia-docker/wiki/Deploy-on-Amazon-EC2
+# Based on Jonathan Calmels approach https://github.com/NVIDIA/nvidia-docker/wiki/Deploy-on-Amazon-EC2
 
-# Install NVIDIA drivers 361.42
-sudo apt-get install --no-install-recommends -y gcc make libc-dev
-wget -P /tmp http://us.download.nvidia.com/XFree86/Linux-x86_64/361.42/NVIDIA-Linux-x86_64-361.42.run
-sudo sh /tmp/NVIDIA-Linux-x86_64-361.42.run --silent
+# Install official NVIDIA driver package
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+sudo apt-get update && sudo apt-get install -y --no-install-recommends cuda-drivers
 
 # Install nvidia-docker and nvidia-docker-plugin
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0/nvidia-docker_1.0.0-1_amd64.debb
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0/nvidia-docker_1.0.0-1_amd64.deb
 sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 
 # Tests install
